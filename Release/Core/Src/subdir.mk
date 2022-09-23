7015e0852063ef8390cb66b478908a1110fb2bf7
Mesih Çelik <39157110+mesihcelik@users.noreply.github.com>
2022-09-23 22:40:34 +0300

    Add files via upload

---
+++ Release/Core/Src/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/4Digit_Driver.c \
+../Core/Src/PC5_Button_SaatDigit_Gecis_SaniyeDigit.c \
+../Core/Src/buttondriver.c \
+../Core/Src/main.c \
+../Core/Src/stm32l4xx_hal_msp.c \
+../Core/Src/stm32l4xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32l4xx.c \
+../Core/Src/timer6_driver.c 
+
+OBJS += \
+./Core/Src/4Digit_Driver.o \
+./Core/Src/PC5_Button_SaatDigit_Gecis_SaniyeDigit.o \
+./Core/Src/buttondriver.o \
+./Core/Src/main.o \
+./Core/Src/stm32l4xx_hal_msp.o \
+./Core/Src/stm32l4xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32l4xx.o \
+./Core/Src/timer6_driver.o 
+
+C_DEPS += \
+./Core/Src/4Digit_Driver.d \
+./Core/Src/PC5_Button_SaatDigit_Gecis_SaniyeDigit.d \
+./Core/Src/buttondriver.d \
+./Core/Src/main.d \
+./Core/Src/stm32l4xx_hal_msp.d \
+./Core/Src/stm32l4xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32l4xx.d \
+./Core/Src/timer6_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32L476xx -c -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/4Digit_Driver.d ./Core/Src/4Digit_Driver.o ./Core/Src/4Digit_Driver.su ./Core/Src/PC5_Button_SaatDigit_Gecis_SaniyeDigit.d ./Core/Src/PC5_Button_SaatDigit_Gecis_SaniyeDigit.o ./Core/Src/PC5_Button_SaatDigit_Gecis_SaniyeDigit.su ./Core/Src/buttondriver.d ./Core/Src/buttondriver.o ./Core/Src/buttondriver.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32l4xx_hal_msp.d ./Core/Src/stm32l4xx_hal_msp.o ./Core/Src/stm32l4xx_hal_msp.su ./Core/Src/stm32l4xx_it.d ./Core/Src/stm32l4xx_it.o ./Core/Src/stm32l4xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32l4xx.d ./Core/Src/system_stm32l4xx.o ./Core/Src/system_stm32l4xx.su ./Core/Src/timer6_driver.d ./Core/Src/timer6_driver.o ./Core/Src/timer6_driver.su
+
+.PHONY: clean-Core-2f-Src
+
